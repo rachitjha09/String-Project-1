bc918ed0e2254472782ded7d41eeefa19444ddc1
rachitjha09 <rachitjha09@gmail.com>
2024-09-02 19:39:00 -0400

    Add files via upload
    CompareTo String class example

---
+++ Demo.java
@@ -0,0 +1,14 @@
+
+/**
+ * Work on the String class
+ * @author (Rachit Jha)
+ * @version (10/4/23)
+ */
+public class Demo
+{
+    public static void main(String[]args){
+        String a="aaa";
+        String b="ar";
+        System.out.println(b.compareTo(a));
+    }
+}
